bd69f1435e990864b607990d7a8f6ef153c4d9be
jochim-uke <jochim.reinert@gmail.com>
2026-09-04 20:12:05 +0200

    Add dienstplan/config.js for duty roster viewer

---
+++ dienstplan/config.js
@@ -0,0 +1,6 @@
+window.DIENSTPLAN_CONFIG={
+  supabaseUrl:"https://tjjtemvclcgqmuhlnumr.supabase.co",
+  publishableKey:"sb_publishable_ZYbal6vsT-gcmAHu-eugKw_h7Fif-1x",
+  functionName:"dienstplan-api",
+  bucket:"dienstplaene"
+};
